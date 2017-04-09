0b86a35d78dd443a952ffec84e8db707208b7738
Theodor Serbana <theodor.serbana96@gmail.com>
2017-04-09 14:45:10 +0300

    Finish up initial UI

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,10 +1,65 @@
 #include "MainWindow.h"
 
 MainWindow::MainWindow() :
-    QDialog()
+    QDialog(),
+    m_enterTextLabel(new QLabel(this)),
+    m_textBox(new QLineEdit(this)),
+    m_computeBtn(new QPushButton(this))
 {
-    // set the title, size, and window buttons
+    // setup the main window
     this->setWindowTitle("RegexToNFA");
-    this->setFixedSize(200, 200);
+    this->setFixedSize(200, 150);
     this->setWindowFlags(this->windowFlags() & ~Qt::WindowContextHelpButtonHint);
+
+    // setup the label
+    m_enterTextLabel->setText("Enter a REGEX in the textbox below:");
+    m_enterTextLabel->move(10, 30);
+
+    // setup the textbox
+    m_textBox->setGeometry(10, 60, 180, 20);
+
+    // setup the button
+    m_computeBtn->setText("Compute the NFA");
+    m_computeBtn->move(50, 100);
+
+    // check when the regex becomes valid, activate the button
+    this->connect(m_textBox, &QLineEdit::textChanged, this, [=]
+    {
+        m_computeBtn->setEnabled(isRegexValid());
+    });
+
+    // when button is clicked (so regex is implicitly valid), magic happens
+    this->connect(m_computeBtn, &QPushButton::clicked, this, [=]
+    {
+        this->close();
+    });
+}
+
+bool MainWindow::isRegexValid()
+{
+    // this validation method is rather naive
+    // and does not take into account the actual validity
+    // of the string as a regex, but it's good to prevent mistakes
+
+    static QString C_ACCEPTED_CHARACTERS("()+*");
+
+    for (const auto& itr : m_textBox->text())
+    {
+        if (itr.isLetter())
+        {
+            if (!itr.isLower())
+            {
+                return false;
+            }
+
+            continue;
+        }
+
+        if (!C_ACCEPTED_CHARACTERS.contains(itr))
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
--- MainWindow.h
+++ MainWindow.h
@@ -3,11 +3,20 @@
 
 #include <QDialog>
 #include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
 
 class MainWindow : public QDialog
 {
 public:
     MainWindow();
+
+private:
+    bool isRegexValid();
+
+    QLabel*    m_enterTextLabel;
+    QLineEdit* m_textBox;
+    QPushButton* m_computeBtn;
 };
 
 #endif // MAINWINDOW_H
